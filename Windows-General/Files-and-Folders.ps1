d547dd37a4f44ab580f727d59dfff845833b3f4f
Jean-Paul van Ravensberg <jvravensberg@users.noreply.github.com>
2016-01-06 16:25:10 +0100

    Create Files-and-Folders.ps1

---
+++ Windows-General/Files-and-Folders.ps1
@@ -0,0 +1,30 @@
+Import-module ActiveDirectory
+$Users = Get-ADGroupMember Lync2013 -Recursive | % SamAccountName | Sort
+
+Foreach ($User in $Users) {
+$ModDate = $Null
+$Source = "\\ManagementServer\C$\Scripts\Start-SkypeForBusiness\Start Skype for Business.bat"
+$Destination = "\\FileServer\Users$\$User\data\startmenu\Programs\Startup\Start Skype for Business.bat"
+
+Write-Output "################"
+Write-Output "Processing user $User now..."
+
+If (Test-Path $Destination) {
+$ModDate = Get-Item "$Destination" | % LastWriteTime
+Write-Output "Modification date before copying: $ModDate"
+}
+
+Else {
+Write-Output "File doesn't exist. Copying now..."
+}
+
+Write-Output "Changing file for $User in $Destination"
+Copy-Item $Source $Destination
+
+$ModDate = Get-Item "$Destination" | % LastWriteTime
+Write-Output "Modification date after copying: $ModDate"
+
+Write-Output "Finished processing user $User..."
+Write-Output "################"
+Write-Output ""
+}
