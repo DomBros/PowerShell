77d9fef30b20a92a83e9b5a0898b121e565c659d
Jean-Paul van Ravensberg <jvravensberg@users.noreply.github.com>
2016-04-29 15:25:14 +0200

    New temp folder location

--- Windows-General/Applications/Deploy-EPIServer.ps1
+++ Windows-General/Applications/Deploy-EPIServer.ps1
@@ -19,7 +19,7 @@ Write-Output "--- Starting script at $(Get-Date -Format "dd-MM-yyyy HH:mm") ---"
 
 # Variables
 #region Variables
-$TempFolder = "C:\Temp\EpiserverDownloads"
+$TempFolder = "$env:TEMP\$((New-GUID).Guid)"
 $WMFSource = "https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu"
 
 $AppDownloads=@{
