433339b3ade7bd84ffcd19e8b9c71d7bfd1929ae
Jean-Paul van Ravensberg <jvravensberg@users.noreply.github.com>
2016-05-12 11:21:12 +0200

    Update Deploy-Episerver.ps1

--- Windows-General/Applications/Deploy-Episerver.ps1
+++ Windows-General/Applications/Deploy-Episerver.ps1
@@ -119,7 +119,7 @@ function Disable-InternetExplorerESC {
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name iexplore -Force
+    Stop-Process -Name iexplore -Force -ErrorAction SilentlyContinue
     Write-Output "IE Enhanced Security Configuration (ESC) has been disabled."
 }
 #endregion
