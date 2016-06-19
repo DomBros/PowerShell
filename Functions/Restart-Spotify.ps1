d4514123b9c3b0c1019388545164b93a273bb545
Jean-Paul van Ravensberg <jvravensberg@users.noreply.github.com>
2016-06-19 10:03:36 +0200

    Update Restart-Spotify.ps1

--- Functions/Restart-Spotify.ps1
+++ Functions/Restart-Spotify.ps1
@@ -1,4 +1,4 @@
-# Restart Spotify when crashed
+# Restart Spotify
 Function Restart-Spotify {
     Get-Process *spotify | Stop-Process
     Start "C:\Users\j.van.ravensberg\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Spotify.lnk"
